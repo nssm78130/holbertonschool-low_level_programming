5d19efe25dcf7458e549f14078091ceffe34f6db
nssm78130 <nassimmameche@icloud.com>
2024-10-25 09:26:07 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,13 +2,13 @@
 /**
  * main - Entry point
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
+ * Description: Printf "with proper grammar, but the outcome is a piece of art"
  * using the puts function, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+}	
